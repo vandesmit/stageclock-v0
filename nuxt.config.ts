22ad7072c63f668bd1bd5f7d2a6d51ac2849acc2
martijnsmitt <martijnsmitt@gmail.com>
2022-07-27 23:10:08 +0200

    start using pages directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,11 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   typescript: {
     shim: false
   },
   modules: ['@nuxtjs/tailwindcss'],
   serverMiddleware: [
-    // Will register file from project server-middleware directory to handle /server-api/* requests
     { path: "/server-api", handler: "~/server-middleware/api.ts" },
   ],
 })
