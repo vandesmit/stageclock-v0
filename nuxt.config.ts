9e49bf423a28c051f001b6da1bd444cb2c9e8447
martijnsmitt <martijnsmitt@gmail.com>
2022-08-05 14:09:29 +0200

    bugfix head usage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,25 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
-  head: {
-    link: [
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
-    ],
-    meta: [
-      { name: 'apple-mobile-web-app-capable', content: 'yes' },
-      { name: 'apple-mobile-web-app-title', content: 'Stage Clock' },
-      { name: 'application-name', content: 'Stage Clock' },
-      { name: 'msapplication-TileColor', content: '#da532c' },
-      { name: 'theme-color', content: '#1e293b' },
-    ],
-    viewport: 'width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover, user-scalable=no',
-    title: 'Stage Clock',
+  app: {
+    head: {
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      ],
+      meta: [
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-title', content: 'Stage Clock' },
+        { name: 'application-name', content: 'Stage Clock' },
+        { name: 'msapplication-TileColor', content: '#da532c' },
+        { name: 'theme-color', content: '#1e293b' },
+      ],
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover, user-scalable=no',
+      title: 'Stage Clock',
+    },
   },
   modules: ['@nuxtjs/tailwindcss'],
   serverMiddleware: [
