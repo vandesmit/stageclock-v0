61d48146cc6580012ae560406af9562ca34b3271
martijnsmitt <martijnsmitt@gmail.com>
2022-08-05 13:15:55 +0200

    add start url for adding full screen experience

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'manifest', href: '/manifest.json' },
     ],
   },
   modules: ['@nuxtjs/tailwindcss'],
