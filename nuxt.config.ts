3771c4d6de3571f309890f6cd3bc8e35203d4b39
martijnsmitt <martijnsmitt@gmail.com>
2022-08-05 13:07:52 +0200

    Add Favicon and webmanifest for A2HS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
+  head: {
+    link: [
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+    ],
+  },
   modules: ['@nuxtjs/tailwindcss'],
   serverMiddleware: [
     { path: "/server-api", handler: "~/server-middleware/api.ts" },
