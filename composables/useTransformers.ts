401e822e5a162154312ea4cc54b918345079e6a3
martijnsmitt <martijnsmitt@gmail.com>
2022-08-11 16:47:07 +0200

    Added readable time func to transform composable

--- composables/useTransformers.ts
+++
@@ -1,6 +0,0 @@
-export const useTransformers = () => {
-  return {
-    transformToPositiveDoubleDigit: (digit: number): string => ('0' + Math.abs(digit)).slice(-2),
-    transformToDoubleDigit: (digit: number): string => ('0' + digit).slice(-2)
-  }
-}
